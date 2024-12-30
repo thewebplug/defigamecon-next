1c5a57e9d68c7e40aab89c71da76f034d8af0a4b
saleem.jibril <saleemjibril5@gmail.com>
2024-12-30 12:24:55 +0100

    chore: defigamecon conversion

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,55 +1,57 @@
 "use client"
-import Header from "../components/header";
-import Explore from "../components/explore";
-import Footer from "../components/footer";
-import EventCard from "../components/eventCard";
-import { getAllEvents } from "@/app/apis";
-import { Suspense, useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import EventCard from "../components/eventCard";
 
-export default function Events() {
-  const auth = useSelector((state) => state.auth);
+function SearchResults({ auth }) {
   const [events, setEvents] = useState([]);
-
   const searchParams = useSearchParams();
   const query = Object.fromEntries(searchParams.entries());
-  
   const event = query?.event;
-  
 
   const handleGetEvents = async () => {
     const response = await getAllEvents(auth?.token);
     console.log("getAllEvents", response?.data[0]?.title, event);
     if(response?.status === 200) {
-      setEvents(response?.data?.filter((item) => item?.title?.toLowerCase()?.includes(event) || item?.venue?.toLowerCase()?.includes(event) || item?.description?.toLowerCase()?.includes(event) || item?.categories?.includes(event)));
+      setEvents(response?.data?.filter((item) => 
+        item?.title?.toLowerCase()?.includes(event) || 
+        item?.venue?.toLowerCase()?.includes(event) || 
+        item?.description?.toLowerCase()?.includes(event) || 
+        item?.categories?.includes(event)
+      ));
     }
-    };
+  };
 
   useEffect(() => {
     handleGetEvents();
   }, [event]);
 
-  
+  return (
+    <div className="events__grid">
+      {events?.map((event) => (
+        <EventCard event={event} key={event?._id} />
+      ))}
+    </div>
+  );
+}
+
+// Main component
+export default function Events() {
+  const auth = useSelector((state) => state.auth);
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <>
       <br />
       <br />
       <Header />
       <main className="events">
-      <div className="events__title">Search results</div>
-
-       <div className="events__grid">
-                {events?.map((event) => (
-                  <EventCard event={event} key={event?._id} />
-                ))}
-              </div>
-            
+        <div className="events__title">Search results</div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <SearchResults auth={auth} />
+        </Suspense>
       </main>
-
       <Explore />
       <Footer />
-    </Suspense>
+    </>
   );
 }
